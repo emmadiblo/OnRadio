528a5da605f4ab25386bb0c1973e3ad284fe8839
emmadiblo <141231760+emmadiblo@users.noreply.github.com>
2024-10-09 16:02:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ $(function () {
     buffInterval = null,
     tFlag = false,
     albums = [ 
+      "RTNB chaine 1",
       "Radio Agakiza",
       "Radio Isanganiro",
       "Radio Maria Burundi",
@@ -41,22 +42,23 @@ $(function () {
       "Antenna web Gitega",
       "Irebe FM",
       "Radio Intwali FM",
-      "Gods Country", 
-      "Save Me",
-      "Numb",
-      "Im Fine",
-      "Energy",
-      "Hey There Delilah",
-      "7 Years",
-      "Wrecked",
-      "Heroes", 
-      "Lose Yourself",
-      "World's Smallest Violin",
-      "Do It All For You",
-      "When I'm Gone",
-      "I Love The Sound Of Silence"
+      "Radio black FM",
+     "Ijwi Ry'Amerika",
+      "Umuco Fm",
+      "Izere FM",
+      "Iteka FM",
+      "Radio Peace FM",
+      "ATOCO Media",
+      "Bdusa Media",
+      "Radio B.A.A MEDIA FM",
+      "Hero Radio (burundi)",
+      " Menya Live",
+      "Radio Inkinzo",
+     "EAGLE SPORTS FM",
+     "Radio voix d'Espoir",
     ],
     trackNames = [
+      "RTNB chaine 1",
       "Agakiza",
       "Isanganiro",
       "Radio Maria Burundi",
@@ -67,23 +69,25 @@ $(function () {
       "Antenna web Gitega",
       "Irebe FM",
       "Radio Intwali FM",
-      "Blake Shelton",
-      "Jelly Roll",
-      "Ryan Oakes",
-      "Ryan Oakes",
-      "Ryan Oakes",
-      "Plain White T's",
-      "Lukas Graham",
-      "Imagine Dragons",
-      "Marshmello & Alan Walker",
-      "Eminem",
-      "AJR",
-      "Alan Walker ft. Trevor Guthrie",
-      "Eminem",
-      "Eminem & Disturbed"
+      "Radio black FM",
+        "Ijwi Ry'Amerika",
+        "Umuco Fm",
+        "Izere FM",
+        "Iteka FM",
+        "Radio Peace FM",
+        "ATOCO Media",
+        "Bdusa Media",
+        "Radio B.A.A MEDIA FM",
+         "Hero Radio (burundi)",
+        " Menya Live",
+        "Radio Inkinzo",
+        "EAGLE SPORTS FM",
+        "Radio voix d'Espoir",
     ],
     albumArtworks = ["_1", "_2", "_3", "_4", "_5","_6","_7","_8","_9","_10","_11","_12","_13","_14","_15","_16","_17","_18","_19","_20","_21","_22","_23","_24"],
-    trackUrl = [ "https://cast6.asurahosting.com/proxy/radioaga/stream",
+    trackUrl = [ 
+      "https://isanganiro.ice.infomaniak.ch/isanganiro-64.mp3",
+      "https://cast6.asurahosting.com/proxy/radioaga/stream",
       "https://isanganiro.ice.infomaniak.ch/isanganiro-64.mp3?_=1",
       "http://dreamsiteradiocp2.com:8082/stream",
       "http://a9.asurahosting.com:8950/radio.mp3",
@@ -98,6 +102,21 @@ $(function () {
 "https://stream.zeno.fm/5wjpe2wxzdeuv",
       
 "https://stream.zeno.fm/f4btyyc18vzuv",
+"https://liveonlineradio.net/radio-black-fm-949",
+"https://n12.radiojar.com/hzcgfqas92quv?rj-ttl=5&rj-tok=AAABknESC5kA4XbvQBp4nhRAHQ",
+"http://s2.free-shoutcast.com:18208/stream",
+"http://5.189.189.39:8000/stream",
+"https://streams.radio.co/sd1bcd1376/listen",
+"https://stream.zeno.fm/s0244utefg8uv",
+"https://stream.zeno.fm/g4x4bb7uxd0uv",
+"https://stream.zeno.fm/0fpegrf70fctv",
+"https://stream.zeno.fm/ud15nzqn0c9uv",
+"https://a2.asurahosting.com:6790/burundi.mp3",
+"https://stream.zeno.fm/fq6c2xsn0c9uv",
+"https://stream.zeno.fm/x3ktdppgx98uv",
+"https://audio.bfmtv.com/rmcradio_128.mp3",
+"https://stream4.rcast.net/65428",
+
 ],
     playPreviousTrackButton = $("#play-previous"),
     playNextTrackButton = $("#play-next"),
@@ -330,190 +349,6 @@ $(document).keyup(function (event) {
 
 initPlayer();
 
-
-var URL =
-"https://s3-us-west-2.amazonaws.com/s.cdpn.io/222579/Kevin_MacLeod_-_Camille_Saint-Sans_Danse_Macabre_-_Finale.mp3";
-var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-var analizador = audioCtx.createAnalyser();
-analizador.fftSize = 1024; // [32, 64, 128, 256, 512, 1024, 2048]
-var dataArray = new Uint8Array(analizador.frequencyBinCount);
-var audioBuffer, fuenteDeReproduccion;
-var stop = true;
-var tiempo = 0,
-progreso = 0;
-var progreso = 0;
-
-//canvas vars
-var canvas = document.querySelector("canvas");
-ctx = canvas.getContext("2d");
-var cw = (canvas.width = window.innerWidth),
-cx = cw / 2;
-var ch = (canvas.height = window.innerHeight),
-cy = ch / 2;
-var R = 150;
-var r = 80;
-var da = 2; // delta angle
-var cos = Math.cos(da * Math.PI / 180);
-var sin = Math.sin(da * Math.PI / 180);
-var requestId = null;
-
-// A U D I O
-
-function solicitarAudio(url) {
-var request = new XMLHttpRequest();
-request.open("GET", url, true);
-request.responseType = "arraybuffer";
-request.onload = function() {
-  boton.style.display = "block";
-  audioCtx.decodeAudioData(request.response, function(buffer) {
-    audioBuffer = buffer;
-  });
-};
-request.send();
-}
-
-function reproducirAudio() {
-fuenteDeReproduccion = audioCtx.createBufferSource();
-fuenteDeReproduccion.buffer = audioBuffer;
-fuenteDeReproduccion.connect(analizador);
-analizador.connect(audioCtx.destination);
-fuenteDeReproduccion.start(audioCtx.currentTime, progreso);
-}
-
-function detenerAudio() {
-fuenteDeReproduccion.stop();
-}
-
-function audio() {
-if (stop) {
-  // si el audio está parado
-  tiempo = audioCtx.currentTime - progreso;
-
-  stop = false;
-  boton.innerHTML = "Stop";
-  reproducirAudio(progreso);
-} else {
-  // de lo contrario
-  stop = true;
-  boton.innerHTML = "Click";
-  detenerAudio();
-}
-}
-
-solicitarAudio(URL);
-
-// Utiliza el evento click para iniciar o detener la reproducción
-boton.addEventListener("click", audio, false);
-
-// C A N V A S
-
-function Barr(a) {
-this.a = a * Math.PI / 180; // angulo
-
-this.dr = 0;
-
-this.cos = Math.cos(this.a);
-this.sin = Math.sin(this.a);
-
-this.draw = function(R, color) {
-  this.x0 = (R + this.dr) * this.cos;
-  this.y0 = (R + this.dr) * this.sin;
-  this.x1 = this.x0 * cos - this.y0 * sin;
-  this.y1 = this.x0 * sin + this.y0 * cos;
-  this.x3 = (R - this.dr) * this.cos;
-  this.y3 = (R - this.dr) * this.sin;
-  this.x2 = this.x3 * cos - this.y3 * sin;
-  this.y2 = this.x3 * sin + this.y3 * cos;
-
-  ctx.fillStyle = lGrd(this.x1, this.y1, this.x2, this.y2, color);
-
-  ctx.beginPath();
-  ctx.moveTo(this.x0, this.y0);
-  ctx.lineTo(this.x1, this.y1);
-  ctx.lineTo(this.x2, this.y2);
-  ctx.lineTo(this.x3, this.y3);
-  ctx.closePath();
-  ctx.fill();
-};
-}
-
-function update(Ry, R, divisor, n, color) {
-for (var i = 0; i < Ry.length; i++) {
-  var dr = dataArray[i * n];
-  Ry[i].dr = dr * dr * dr / divisor;
-  Ry[i].draw(R, color);
-}
-}
-
-// los arrays de barras
-var Ry = [];
-var Ry1 = [];
-
-for (var i = 0; i < 180; i += 2 * da) {
-var b = new Barr(i);
-Ry.push(b);
-}
-// la parte reflejada
-for (var i = -2 * da; i > -(180 + 2 * da); i -= 2 * da) {
-var b = new Barr(i);
-Ry1.push(b);
-}
-
-//ctx.translate(cx, cy);
-//ctx.rotate(-Math.PI / 2);
-
-function Animacion() {
-requestId = window.requestAnimationFrame(Animacion);
-analizador.getByteFrequencyData(dataArray);
-ctx.clearRect(-cw, -ch, 2 * cw, 2 * ch);
-var n = ~~(analizador.frequencyBinCount / Ry.length);
-
-update(Ry, R, 25000, n, "hsla(178,96%,10%,1)");
-update(Ry1, R, 25000, n, "hsla(178,96%,10%,1)");
-
-update(Ry, r, 200000, n, "#039691");
-update(Ry1, r, 200000, n, "#039691");
-}
-
-function init() {
-if (requestId) {
-  window.cancelAnimationFrame(requestId);
-  requestId = null;
-}
-(cw = canvas.width = window.innerWidth), (cx = cw / 2);
-(ch = canvas.height = window.innerHeight), (cy = ch / 2);
-
-ctx.translate(cx, cy);
-ctx.rotate(-Math.PI / 2);
-// llama la función fotograma para iniciar la animación
-Animacion();
-}
-
-function lGrd(x, y, x1, y1, color) {
-var grd = ctx.createLinearGradient(x, y, x1, y1);
-grd.addColorStop(0, "black");
-grd.addColorStop(0.5, color);
-grd.addColorStop(1, "black");
-return grd;
-}
-
-window.setInterval(function() {
-init();
-
-window.addEventListener("resize", init, false);
-
-// si el audio se está reproduciendo
-if (stop == false) {
-  // calcula el progreso del audio en segundos
-  progreso = audioCtx.currentTime - tiempo;
-}
-
-if (audioBuffer && audioCtx.currentTime - tiempo >= audioBuffer.duration) {
-  stop = true;
-  boton.innerHTML = "Click";
-  progreso = 0;
-}
-}, 1000 / 30);
 
 
 });
